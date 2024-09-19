d39e0a04dccb2b565350d1c21776a8293e1bd19b
Marchenko <aleks.marchenko802@gmail.com>
2024-09-19 17:10:29 +0300

    enable autoloading of bootstrap files

--- twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/runner/impl/MockKafkaStreamRunner.java
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter/to/kafka/service/runner/impl/MockKafkaStreamRunner.java
@@ -22,7 +22,7 @@ import java.util.concurrent.ThreadLocalRandom;
 @Component
 public class MockKafkaStreamRunner implements StreamRunner {
 
-    private static final Logger LOG = LoggerFactory.getLogger(TwitterKafkaStreamRunner.class);
+    private static final Logger LOG = LoggerFactory.getLogger(MockKafkaStreamRunner.class);
 
     private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
 
