a0caac5bea26a3c5023003fbe4b944416ff7e4bc
Marchenko <aleks.marchenko802@gmail.com>
2024-09-23 15:53:18 +0300

    add jce

---
+++ config-server/src/main/java/com/microservices/demo/config/server/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.microservices.demo.config.server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(c -> c.ignoringRequestMatchers("/encrypt/**", "/decrypt/**"))
+                .authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/encrypt/**", "/decrypt/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+}
