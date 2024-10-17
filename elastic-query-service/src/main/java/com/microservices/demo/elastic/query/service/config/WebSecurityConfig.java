43a9fbe5d3ab18378227f6caee0b6411140b746f
Marchenko <aleks.marchenko802@gmail.com>
2024-10-17 21:43:42 +0300

    create web controller

---
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package com.microservices.demo.elastic.query.service.config;
+
+import com.microservices.demo.config.UserConfigData;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    private final UserConfigData userConfigData;
+
+    public WebSecurityConfig(UserConfigData userData) {
+        this.userConfigData = userData;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/**").hasRole("USER"))
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username(userConfigData.getUsername())
+                .password(passwordEncoder().encode(userConfigData.getPassword()))
+                .roles(userConfigData.getRoles())
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
