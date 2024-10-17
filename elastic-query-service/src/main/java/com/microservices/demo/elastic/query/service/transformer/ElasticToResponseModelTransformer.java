76fa05b493bf1e8f4185262927c62ecf096c1906
Marchenko <aleks.marchenko802@gmail.com>
2024-10-17 22:00:38 +0300

    create business layer for returning elastic data

---
+++ elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/transformer/ElasticToResponseModelTransformer.java
@@ -0,0 +1,31 @@
+package com.microservices.demo.elastic.query.service.transformer;
+
+import com.microservices.demo.elastic.model.index.impl.TwitterIndexModel;
+import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModel;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ElasticToResponseModelTransformer {
+
+    public ElasticQueryServiceResponseModel getResponseModel(TwitterIndexModel twitterIndexModel) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssZ");
+
+        return ElasticQueryServiceResponseModel
+                .builder()
+                .id(twitterIndexModel.getId())
+                .userId(twitterIndexModel.getUserId())
+                .text(twitterIndexModel.getText())
+                .createdAt(OffsetDateTime.parse(twitterIndexModel.getCreatedAt(), formatter).toLocalDateTime())
+                .build();
+    }
+
+    public List<ElasticQueryServiceResponseModel> getResponseModels(List<TwitterIndexModel> twitterIndexModels) {
+        return twitterIndexModels.stream().map(this::getResponseModel).collect(Collectors.toList());
+    }
+}
