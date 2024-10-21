d15e07069423e97555f43fc4488978407e0a5e6c
Marchenko <aleks.marchenko802@gmail.com>
2024-10-21 19:17:16 +0300

    create reactive elastic web client

---
+++ reactive-elastic-query-web-client/src/test/java/com/microservices/demo/reactive/elastic/query/web/client/ReactiveElasticQueryWebClientApplicationTest.java
@@ -0,0 +1,12 @@
+package com.microservices.demo.reactive.elastic.query.web.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveElasticQueryWebClientApplicationTest {
+
+    @Test
+    public void contextLoads() {
+    }
+}
