e9d5d067074b5ce9ad0cc285027dc85fe78d349c
Marchenko <aleks.marchenko802@gmail.com>
2024-10-28 19:02:16 +0200

    configure web client security

--- elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/config/WebClientConfig.java
+++ elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/config/WebClientConfig.java
@@ -6,13 +6,17 @@ import io.netty.channel.ChannelOption;
 import io.netty.handler.timeout.ReadTimeoutHandler;
 import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.apache.hc.core5.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunctions;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.tcp.TcpClient;
 
@@ -24,36 +28,43 @@ public class WebClientConfig {
 
     private final ElasticQueryWebClientConfigData.WebClient elasticQueryWebClientConfigData;
 
-    private final UserConfigData userConfigData;
-
-    public WebClientConfig(ElasticQueryWebClientConfigData webClientConfigData, UserConfigData userData) {
+    public WebClientConfig(ElasticQueryWebClientConfigData webClientConfigData) {
         this.elasticQueryWebClientConfigData = webClientConfigData.getWebClient();
-        this.userConfigData = userData;
     }
+
+    @Value("${security.default-client-registration-id}")
+    private String defaultClientRegistrationId;
 
     @LoadBalanced
     @Bean("webClientBuilder")
-    WebClient.Builder webClientBuilder() {
+    WebClient.Builder webClientBuilder(ClientRegistrationRepository clientRegistrationRepository,
+                                       OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository) {
+
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+                        clientRegistrationRepository,
+                        oAuth2AuthorizedClientRepository);
+        oauth2.setDefaultOAuth2AuthorizedClient(true);
+        oauth2.setDefaultClientRegistrationId(defaultClientRegistrationId);
+
         return WebClient.builder()
-                .filter(ExchangeFilterFunctions
-                        .basicAuthentication(userConfigData.getUsername(), userConfigData.getPassword()))
                 .baseUrl(elasticQueryWebClientConfigData.getBaseUrl())
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, elasticQueryWebClientConfigData.getContentType())
                 .defaultHeader(HttpHeaders.ACCEPT, elasticQueryWebClientConfigData.getAcceptType())
-                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(getTcpClient())))
+                .clientConnector(new ReactorClientHttpConnector(getHttpClient()))
+                .apply(oauth2.oauth2Configuration())
                 .codecs(clientCodecConfigurer ->
                         clientCodecConfigurer
                                 .defaultCodecs()
                                 .maxInMemorySize(elasticQueryWebClientConfigData.getMaxInMemorySize()));
     }
 
-    private TcpClient getTcpClient() {
-        return TcpClient.create()
+    private HttpClient getHttpClient() {
+        return HttpClient.create()
                 .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, elasticQueryWebClientConfigData.getConnectTimeoutMs())
                 .doOnConnected(connection -> {
-                    connection.addHandlerLast(
-                            new ReadTimeoutHandler(elasticQueryWebClientConfigData.getReadTimeoutMs(),
-                                    TimeUnit.MILLISECONDS));
+                    connection.addHandlerLast(new ReadTimeoutHandler(elasticQueryWebClientConfigData.getReadTimeoutMs(),
+                            TimeUnit.MILLISECONDS));
                     connection.addHandlerLast(
                             new WriteTimeoutHandler(elasticQueryWebClientConfigData.getWriteTimeoutMs(),
                                     TimeUnit.MILLISECONDS));
--- elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/config/WebSecurityConfig.java
+++ elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/config/WebSecurityConfig.java
@@ -1,60 +1,88 @@
 package com.microservices.demo.elastic.query.web.client.config;
 
-import com.microservices.demo.config.UserConfigData;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.OidcUserInfo;
+import org.springframework.security.oauth2.core.oidc.user.OidcUserAuthority;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(WebSecurityConfig.class);
 
-    private final UserConfigData userConfigData;
+    private static final String GROUPS_CLAIM = "groups";
 
-    public WebSecurityConfig(UserConfigData userData) {
-        this.userConfigData = userData;
+    private static final String ROLE_PREFIX = "ROLE_";
+
+    private final ClientRegistrationRepository clientRegistrationRepository;
+
+    public WebSecurityConfig(ClientRegistrationRepository clientRegistrationRepository) {
+        this.clientRegistrationRepository = clientRegistrationRepository;
+    }
+
+    @Value("${security.logout-success-url}")
+    private String logoutSuccessUrl;
+
+    OidcClientInitiatedLogoutSuccessHandler oidcLogoutSuccessHandler() {
+        OidcClientInitiatedLogoutSuccessHandler successHandler =
+                new OidcClientInitiatedLogoutSuccessHandler(clientRegistrationRepository);
+        successHandler.setPostLogoutRedirectUri(logoutSuccessUrl);
+        return successHandler;
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .httpBasic(Customizer.withDefaults())
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/").permitAll()
-                        .requestMatchers("/**").hasRole("USER")
-                        .anyRequest()
-                        .fullyAuthenticated())
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
-                .logout(logout -> logout.logoutSuccessUrl("/")
-                        .invalidateHttpSession(true)
-                        .deleteCookies("JSESSIONID"));
-
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers(new AntPathRequestMatcher("/"))
+                        .permitAll()
+                        .anyRequest().authenticated())
+                .logout(conf -> conf.logoutSuccessHandler(oidcLogoutSuccessHandler()))
+                .oauth2Client(Customizer.withDefaults())
+                .oauth2Login(conf -> conf.userInfoEndpoint(Customizer.withDefaults()));
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.builder()
-                .username(userConfigData.getUsername())
-                .password(passwordEncoder().encode(userConfigData.getPassword()))
-                .roles(userConfigData.getRoles())
-                .build();
+    public GrantedAuthoritiesMapper userAuthoritiesMapper() {
+        return (authorities) -> {
+            Set<GrantedAuthority> mappedAuthorities = new HashSet<>();
 
-        return new InMemoryUserDetailsManager(user);
+            authorities.forEach(
+                    authority -> {
+                        if (authority instanceof OidcUserAuthority) {
+                            OidcUserAuthority oidcUserAuthority = (OidcUserAuthority) authority;
+                            OidcIdToken oidcIdToken = oidcUserAuthority.getIdToken();
+                            LOG.info("Username from id token: {}", oidcIdToken.getPreferredUsername());
+                            OidcUserInfo userInfo = oidcUserAuthority.getUserInfo();
+
+                            List<SimpleGrantedAuthority> groupAuthorities =
+                                    userInfo.getClaimAsStringList(GROUPS_CLAIM).stream()
+                                            .map(group ->
+                                                    new SimpleGrantedAuthority(ROLE_PREFIX + group.toUpperCase()))
+                                            .collect(Collectors.toList());
+                            mappedAuthorities.addAll(groupAuthorities);
+                        }
+                    });
+            return mappedAuthorities;
+        };
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
